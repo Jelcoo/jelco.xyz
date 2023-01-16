12d1553bb200237fa59872b7c2394ddf2f29779e
Jelco <jelcovthull@gmail.com>
2023-01-16 15:48:48 +0100

    Add brand icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.html"],
-    theme: {
-      extend: {},
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-linkedin': '#0a66c2',
+        'orange-gitlab': '#fc6d26',
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
