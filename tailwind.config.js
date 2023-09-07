1ab95e31ef031148ef7dc8b8d76a3860a9a06a8f
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2023-09-07 11:50:04 +0200

    Remove linkedin & twitter

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'blue-linkedin': '#0a66c2',
-        'blue-twitter': '#52cbff',
         'orange-gitlab': '#fc6d26',
       },
     },
